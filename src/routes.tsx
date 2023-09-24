b51fbe455e9b469bd4ccc280d076b4c2b70ff50f
Lucas Lopes <lucas.santos.pessoal@outlook.com>
2023-09-24 09:36:19 -0300

    feat: form para criação de formulários

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,10 +1,15 @@
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import { DefaultPage } from './pages/DefaultPage';
+import { Login } from './pages/Login';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <DefaultPage />,
+  },
+  {
+    path: '/login',
+    element: <Login />,
   },
 ]);
 
