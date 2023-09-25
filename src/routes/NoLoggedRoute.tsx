35ab373bea1bdfa04474d10c98ca685e2064700a
Lucas-Lopes-II <lucas.santos.pessoal@outlook.com>
2023-09-25 12:07:02 -0300

    feat: criação do NoLoggedRoute para login

---
+++ src/routes/NoLoggedRoute.tsx
@@ -0,0 +1,12 @@
+import { Navigate } from 'react-router-dom';
+import { useLoggedUser } from '../store/hooks';
+
+interface NoLoggedRouteProps {
+  children: JSX.Element;
+}
+
+export const NoLoggedRoute = ({ children }: NoLoggedRouteProps) => {
+  const loggedUser = useLoggedUser();
+
+  return !loggedUser ? children : <Navigate to={'/'} />;
+};
--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -2,6 +2,7 @@ import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import DefaultPage from '../pages/DefaultPage';
 import Login from '../pages/Login';
 import { ProtectedRoute } from './ProtectedRoute';
+import { NoLoggedRoute } from './NoLoggedRoute';
 
 const router = createBrowserRouter([
   {
@@ -14,7 +15,11 @@ const router = createBrowserRouter([
   },
   {
     path: '/login',
-    element: <Login />,
+    element: (
+      <NoLoggedRoute>
+        <Login />
+      </NoLoggedRoute>
+    ),
   },
 ]);
 
